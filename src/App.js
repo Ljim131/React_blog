ad9584c9669c7ed32589254dd1aa3cc1d0cf826d
Jims <95473182+Ljim131@users.noreply.github.com>
2023-01-15 00:40:55 +0900

    3 commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '강남우동맛집';
+  let [글제목, b] = useState('남자 코트 추천');
+
   return (
     <div className="App">
-      <div>
-        <h4>수정했어용</h4>
+      <div className="black-nav">
+        <h4 style={ {color : 'red', fontSize : '16px'}}>블로그임</h4>
+      </div>
+      <div className='list'>
+        <h4> { 글제목 } </h4>
+        <p>1월 15일 발행</p>
       </div>
     </div>
   );
