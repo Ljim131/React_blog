7b4c5beeb0ce62d73cb894ccc18f6d8bdcb07310
Jims <95473182+Ljim131@users.noreply.github.com>
2023-01-15 18:56:39 +0900

    4 commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */ //워닝 메시지 없애는 것
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -5,15 +7,31 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남우동맛집';
-  let [글제목, b] = useState('남자 코트 추천');
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);
+  let [좋아요, 좋아요변경] = useState(0);
 
-  return (
+   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={ {color : 'red', fontSize : '16px'}}>블로그임</h4>
+        <h4 style={ {color : 'red', fontSize : '16px'}}>React Blog</h4>
+      </div>
+
+      <button onClick={ () => {
+          let copy = [...글제목];
+          copy[0] = '여자 코트 추천';
+          글제목변경(copy);
+        }}>변해라얍</button>
+
+      <div className='list'>
+        <h4> { 글제목[0] } <span onClick={ () => { 좋아요변경(좋아요+1) } }>👍</span> {좋아요} </h4>
+        <p>1월 15일 발행</p>
       </div>
       <div className='list'>
-        <h4> { 글제목 } </h4>
+        <h4> { 글제목[1] } </h4>
+        <p>1월 15일 발행</p>
+      </div>
+      <div className='list'>
+        <h4> { 글제목[2] } </h4>
         <p>1월 15일 발행</p>
       </div>
     </div>
